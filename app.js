69e1a5353725d713c54dfb3a55024c03cf5d0ab3
Stevenson Nelson <sonson521@gmail.com>
2022-06-22 22:47:55 -0400

    add eventListener

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const form = document.getElementById('guessingForm');
+const numInput = document.getElementById('numInput');
+const submitBtn = document.getElementById('submitBtn');
+let previousCount = document.getElementById('previousGuess');
+let guessesRemaining = document.getElementById('guessesRemaining');
+let scale = document.getElementById('scale');
 
+form.addEventListener('submit', function (e) {
+  console.log('Hello World');
+  e.preventDefault();
+});
