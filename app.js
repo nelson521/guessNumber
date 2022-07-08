900a165342d29da8cdbfb990569d6fa3112a5fdd
Stevenson Nelson <sonson521@gmail.com>
2022-07-08 10:35:44 -0400

    add plus equals

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ let guessesRemaining = document.getElementById('guessesRemaining');
 let scale = document.getElementById('scale');
 
 form.addEventListener('submit', function (e) {
-  console.log('Hello World');
+  let theValue = parseInt(numInput.value);
+  /* 
+
+  - You need to get the value of the input
+  - You need to put the value of the input in previous guess if its wrong 
+  - You need to get a random number between 1-10
+  - Place you win under scale section 
+  - 
+
+  */
+
+  if (theValue === 3) {
+    console.log('volia');
+  } else {
+    previousCount.textContent += theValue;
+  }
+
   e.preventDefault();
 });
